2fffe891d449df971261fb8fc876c1fabea6cb5b
Gautam Kotian <gautam.kotian@gmail.com>
2013-11-20 22:39:13 +0100

    Added time calculation to random key-value pair generation

--- menu_based_app.cpp
+++ menu_based_app.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <sys/time.h>
 
 #include "hashmap.hpp"
 
 const uint32_t MAX_HASHMAP_SIZE = 65536;
 const bool     SHARED_KEYS_ALLOWED = false;
+
+uint64_t getCurTimeInMS()
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+
+    uint64_t ret = tv.tv_usec;
+
+    // Convert microseconds to milliseconds.
+    ret /= 1000;
+
+    // Convert seconds to milliseconds, and add to previous value.
+    ret += (tv.tv_sec * 1000);
+
+    return ret;
+}
 
 std::string getRandomString(size_t length)
 {
@@ -28,7 +46,7 @@ void showMenu()
     std::cout << "4. Print the entire hash map" << std::endl;
     std::cout << "5. Delete the entire hash map" << std::endl;
     std::cout << "6. Print details about the hash map" << std::endl;
-    std::cout << "7. Generate random key-value pairs" << std::endl;
+    std::cout << "7. Generate random key-value pairs, and insert them" << std::endl;
     std::cout << "8. List colliding keys" << std::endl;
     std::cout << "9. Exit" << std::endl;
 }
@@ -115,11 +133,15 @@ int main()
                 std::cout << "    How many key-value pairs to generate? ";
                 std::cin >> numRandomPairs;
 
+                uint64_t startTime = getCurTimeInMS();
+
                 for (uint64_t i = 0; i < numRandomPairs; ++i)
                 {
                     hashMap.insertElement(getRandomString(10), getRandomString(10));
                 }
-                std::cout << "    Done" << std::endl;
+
+                uint64_t endTime = getCurTimeInMS();
+                std::cout << "    Done in approximately " << (endTime - startTime) << " milliseconds" << std::endl;
 
                 break;
             }
